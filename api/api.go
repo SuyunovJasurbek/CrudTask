831188e20ec78790683b3eeeec3d95b9b45748ca
Jasurbek <suyunovjas7053@gmail.com>
2023-12-20 18:45:48 +0500

    cors

--- api/api.go
+++ api/api.go
@@ -2,14 +2,34 @@ package api
 
 import (
 	"github.com/SuyunovJasurbek/CrudTask/src/handler"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func SetUpApi(h *handler.Handler) *gin.Engine {
-	w := gin.Default()
+	w := gin.New()
+	config := cors.DefaultConfig()
+	config.AllowHeaders = append(
+		config.AllowHeaders,
+		`Content-Type,
+		Content-Length,
+		Accept-Encoding,
+		X-CSRF-Token,
+		Authorization,
+		accept,
+		origin,
+		Cache-Control,
+		X-Requested-With`,
+	)
+	config.AllowMethods = append(config.AllowMethods, "OPTIONS")
+	config.AllowAllOrigins = true
+	config.AllowCredentials = true
+	config.AllowHeaders = append(config.AllowHeaders, "*")
 
+	w.Use(cors.New(config))
+	w.Use(MaxAllowed(100))
 	swagger := ginSwagger.URL("/swagger/doc.json") // The url pointing to API definition
 	w.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, swagger))
 	w.GET("/ping", h.Ping)
@@ -38,3 +58,13 @@ func SetUpApi(h *handler.Handler) *gin.Engine {
 	return w
 }
 
+func MaxAllowed(n int) gin.HandlerFunc {
+	sem := make(chan struct{}, n)
+	acquire := func() { sem <- struct{}{} }
+	release := func() { <-sem }
+	return func(c *gin.Context) {
+		acquire()       // before request
+		defer release() // after request
+		c.Next()
+	}
+}
